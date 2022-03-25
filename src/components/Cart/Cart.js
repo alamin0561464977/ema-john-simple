e42c52cdb521a2db10ab866851ffd3d1f27a81ff
alamin0561464977 <amin23435678901315@gmail.com>
2022-03-25 06:01:29 +0300

    Order Summary

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
+    const { cart } = props;
+    console.log(cart)
+    let total = 0;
+    let shipping = 0;
+    for (const products of cart) {
+        total = total + products.price;
+        shipping = shipping + products.shipping;
+        console.log(shipping)
+
+    }
+    let tax = (total * 0.1).toFixed(2);
+    let Tax = parseFloat(tax)
+    let grandTotal = total + shipping + Tax;
+
     return (
-        <div>
+        <div className='cart'>
             <h1>Order Summary</h1>
             <h3>Selected Items: {props.cart.length}</h3>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h3>Grand Total: ${grandTotal}</h3>
         </div>
     );
 };
